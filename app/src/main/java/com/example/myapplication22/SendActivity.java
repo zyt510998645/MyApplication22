b301f29ccac50e3b8e378566f427fdc531756db0
Zhangyt <zyt510998645@gmail.com>
2016-10-21 11:38:48 +0800

    于sendactivity得到了具体的中文地址（对经纬度解析）

--- app/src/main/java/com/example/myapplication22/SendActivity.java
+++ app/src/main/java/com/example/myapplication22/SendActivity.java
@@ -8,6 +8,8 @@ import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v4.app.ActivityCompat;
 import android.util.Log;
 import android.view.View;
@@ -15,6 +17,16 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.net.URL;
 import java.util.List;
 import java.util.StringTokenizer;
 
@@ -22,11 +34,11 @@ import java.util.StringTokenizer;
  * Created by 51099 on 2016/10/17.
  */
 public class SendActivity extends BaseActivity {
-    //public static final int SHOW_LOCATION=0;  // add 1019
-    //Button button=(Button) findViewById(R.id.send);  //add 1019
+    public static final int SHOW_LOCATION=0;  // add 1019
 
     private String TAG="SendActivity";
     private TextView positionTextView;
+    private TextView positionTextView2;//add 10/20
     private LocationManager locationManager;
     private String provider;
     //private Location location2;
@@ -35,7 +47,8 @@ public class SendActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.send);
-
+        Button button=(Button) findViewById(R.id.send);  //add 1019
+        positionTextView2 = (TextView) findViewById(R.id.position_text_view2);//add 10/20
         positionTextView = (TextView) findViewById(R.id.position_text_view);
         locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
         //获取所有可用的位置提供器
@@ -60,6 +73,7 @@ public class SendActivity extends BaseActivity {
         if (location != null) {
             //显示当前设备的位置信息
             showLocation(location);
+            showshowLocation(location);  //add 10/20
             LogUtil.d(TAG,"showLocation(location)");
         }
         locationManager.requestLocationUpdates(provider, 5000, 1, locationListener);
@@ -85,10 +99,12 @@ public class SendActivity extends BaseActivity {
     }
 
     LocationListener locationListener=new LocationListener() {
-
+        public Location location11=null;
         @Override
         public void onStatusChanged(String provider, int status, Bundle extras) {
             LogUtil.d(TAG,"onStatusChanged");
+            showLocation(location11);
+            showshowLocation(location11); //add 10/20
         }
 
         @Override
@@ -102,8 +118,10 @@ public class SendActivity extends BaseActivity {
 
         @Override
         public void onLocationChanged(Location location) {
+            location11 = location;
             //更新当前设备的位置信息
             showLocation(location);
+            showshowLocation(location); //add 10/20
             LogUtil.d(TAG,"onLocationChanged");
         }
     };
@@ -113,4 +131,67 @@ public class SendActivity extends BaseActivity {
         positionTextView.setText(currentPosition);
         LogUtil.d(TAG,"ShowPosition");
     }
+    //add 10/20
+    private void showshowLocation(final Location location){
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    //组装反向地理编码的接口地址
+                    StringBuilder url= new StringBuilder();
+                    url.append("http://maps.googleapis.com/maps/api/geocode/json?latlng=");
+                    url.append(location.getLatitude()).append(",");
+                    //double zhang1=location.getLatitude();
+                    //LogUtil.i(TAG,"zhang"+zhang1);
+                    url.append(location.getLongitude());
+                    url.append("&sensor=false");
+                    HttpClient httpClient=new DefaultHttpClient();
+                    HttpGet httpGet=new HttpGet(url.toString());
+                    //在请求消息中指定语言，保证服务器返回中文数据
+                    httpGet.addHeader("Accept-language","zh-CN");
+                    LogUtil.d(TAG,"333");
+                    HttpResponse httpResponse= httpClient.execute(httpGet);
+                    LogUtil.d(TAG,"33333333333");
+                    LogUtil.d(TAG,"zhang"+httpResponse.getStatusLine().getStatusCode());
+                    if(httpResponse.getStatusLine().getStatusCode()==200){
+                        LogUtil.d(TAG,"4444");
+                        HttpEntity entity= httpResponse.getEntity();
+                        String response= EntityUtils.toString(entity,"utf-8");
+                        JSONObject jsonObject=new JSONObject(response);
+                        //获取results节点下的位置信息
+                        JSONArray resultArray=jsonObject.getJSONArray("results");
+                        LogUtil.d(TAG,"44444444444");
+                        if (resultArray.length() > 0){
+                            LogUtil.d(TAG,"55555555555");
+                            JSONObject subObject =resultArray.getJSONObject(0);
+                            //取出格式化后的位置信息
+                            String address = subObject.getString("formatted_address");
+                            Message message=new Message();
+                            message.what = SHOW_LOCATION;
+                            message.obj = address;
+                            SendActivity.this.handler.sendMessage(message);
+                            LogUtil.d(TAG,"had sendMessage to handler!!!!!!!!!");
+                        }
+                    } else {
+                        LogUtil.e(TAG, "cannot !!!!!!!!!!!!");
+                    }
+                } catch (Exception e){
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+    }
+
+    private Handler handler=new Handler(){
+      public void handleMessage(Message msg){
+          switch (msg.what){
+              case SHOW_LOCATION:
+                  String currentPosition = (String) msg.obj;
+                  positionTextView2.setText(currentPosition);
+                  break;
+              default:
+                  break;
+          }
+      }
+    };
 }
